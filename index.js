f68ada0c120d05079b3992152506a200139b2389
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-08-18 21:46:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Santiago Calatrava';
+//Santiago Calatrava - acclaimed Spanish/Swiss architect, structural engineer, sculptor and painter.
+//const welcomeMessage = 'Welcome to Flatbook, '+ currentUser + '!';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; //template literal
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = `Welcome, ${currentUser[0]}!`; using bracket notation to access the first value, 0, of the string
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
